ca5dc874155333a3cc1157f225cb9a30ee028083
Nathan <nathan.altomare.belier@gmail.com>
2021-02-25 18:27:55 +0100

    Reworking, fixing, trying to avoid the spaghetti monster

--- js/app.js
+++ js/app.js
@@ -72,7 +72,7 @@ class bowlingGameManager {
     savePlayersName(numberOfPlayers) {
         for (let i = 0; i < numberOfPlayers; i++) {
             const input = document.getElementById("input_" + i).value;
-            const playerInformation = { name: input, launchHistory: [], scoreByFrames: [] }
+            const playerInformation = { name: input, launchHistory: [], scoreByFrames: [], index: 0 }
             this.playersInformations.push(playerInformation);
         }
     }
@@ -90,8 +90,9 @@ class bowlingGameManager {
 
             buttonAddLaunch.addEventListener("click", (event) => {
                 const score = parseInt(document.getElementsByClassName("playerScoreTable_ScoreSelect")[i].value)
-                console.log(score)
+                    // console.log(score)
                 this.addLaunch(i, score);
+                this.frameScoreCalcul(i);
                 this.frameScoreCalcul(i);
                 event.preventDefault();
             });
@@ -99,12 +100,13 @@ class bowlingGameManager {
     }
 
     addLaunch(playerNumero, fallenPins) {
-        const scoreTable = this.playersInformations[playerNumero].launchHistory;
-        const scoreTableLength = scoreTable.length;
+        const launchHistory = this.playersInformations[playerNumero].launchHistory;
         const printScoreTable = document.getElementsByClassName("allLaunchShow")[playerNumero];
-        const td = printScoreTable.getElementsByTagName("td")[scoreTableLength]
+        const td = printScoreTable.getElementsByTagName("td")
+        let numberOfStrikes = 0;
+        let positionInTheTableOnTheScreen;
 
-        if (scoreTableLength >= 21) {
+        if (launchHistory.length >= 21) {
             console.log("ERREUR : VOUS AVEZ DEJA TROP JOUER")
             return
         }
@@ -114,78 +116,76 @@ class bowlingGameManager {
             return
         }
 
-        if (scoreTableLength % 2 === 0 && fallenPins === 10) { // C'EST UN STRIKE ??
-
-            scoreTable.push("X", "X");
-            console.log(BGM.playersInformations[playerNumero].launchHistory)
-
-            printScoreTable.getElementsByTagName("td")[scoreTableLength + 1].innerHTML = "X"
-
-            return
+        for (let i = 0; i < launchHistory.length; i++) {
+            if (launchHistory[i] == "X") {
+                numberOfStrikes++
+            }
         }
 
-        if (scoreTableLength % 2 != 0) { // SECOND LANCER DU FRAMES
-            const previousLaunch = scoreTable[scoreTableLength - 1]
-            console.log(previousLaunch)
+        positionInTheTableOnTheScreen = launchHistory.length + numberOfStrikes
 
-            if ((fallenPins + previousLaunch) > 10) {
+        if (positionInTheTableOnTheScreen % 2 != 0) { // SECOND LANCER DU FRAMES
+            const previousLaunch = launchHistory[launchHistory.length - 1]
+
+            if ((fallenPins + previousLaunch) > 10 || previousLaunch == "X") {
                 console.log("ERREUR IL N'Y A QUE 10 QUILLES")
                 return
             }
         }
 
+        if (fallenPins === 10) {
+            if (launchHistory[launchHistory.length - 1] == 0) {
+                launchHistory.push(10);
+                printScoreTable.getElementsByTagName("td")[positionInTheTableOnTheScreen].innerHTML = "/"
+            } else {
+                launchHistory.push("X");
+                printScoreTable.getElementsByTagName("td")[positionInTheTableOnTheScreen + 1].innerHTML = "X"
+            }
+            return
+        }
+        td[positionInTheTableOnTheScreen].innerHTML = fallenPins;
 
-        scoreTable.push(fallenPins);
-        td.innerHTML = fallenPins;
-        console.log(BGM.playersInformations[playerNumero].launchHistory)
+        launchHistory.push(fallenPins);
     }
 
     frameScoreCalcul(playerNumero) {
-        const scoreTable = this.playersInformations[playerNumero].launchHistory;
+        const launchHistory = this.playersInformations[playerNumero].launchHistory;
+        const index = this.playersInformations[playerNumero].index;
         const scoreByFrames = this.playersInformations[playerNumero].scoreByFrames;
-        const numberOfFrame = scoreByFrames.length * 2;
         const printScoreTable = document.getElementsByClassName("frames")[playerNumero];
-        const td = printScoreTable.getElementsByTagName("td")[scoreByFrames.length];
+        const td = printScoreTable.getElementsByTagName("td");
 
-        if (scoreTable[numberOfFrame] != null && scoreTable[numberOfFrame + 1] != null) { //(FRAMES EST JOUER)
-
-            if (scoreTable[numberOfFrame] + scoreTable[numberOfFrame + 1] === 10) { // (SPARE)
-                if (scoreTable[numberOfFrame + 2] != null) {
-                    if (scoreTable[numberOfFrame + 2] == "X") {
-                        scoreByFrames.push((scoreTable[numberOfFrame] + scoreTable[numberOfFrame + 1] + 10))
-                        td.innerHTML = scoreByFrames[scoreByFrames.length - 1]
+        if (launchHistory[index] == "X") { // STRIKES
+            if (launchHistory[index] != null && launchHistory[index + 1] != null && launchHistory[index + 2] != null) {
+                let sum = 0;
+                for (let i = 0; i < 3; i++) {
+                    if (launchHistory[index + i] == "X") {
+                        sum += 10
                     } else {
-                        scoreByFrames.push((scoreTable[numberOfFrame] + scoreTable[numberOfFrame + 1] + scoreTable[numberOfFrame + 2]))
-                        td.innerHTML = scoreByFrames[scoreByFrames.length - 1]
+                        sum += launchHistory[index + i]
                     }
+                }
+                scoreByFrames.push(sum)
+                this.playersInformations[playerNumero].index += 1;
+                td[scoreByFrames.length - 1].innerHTML = scoreByFrames[scoreByFrames.length - 1]
+            }
+            return
+        } else if (launchHistory[index] != null && launchHistory[index + 1] != null) {
+            if ((launchHistory[index] + launchHistory[index + 1]) == 10) {
+                if (launchHistory[index + 2] != null) {
+                    if (launchHistory[index + 2] == "X") {
+                        scoreByFrames.push(20)
+                    } else {
+                        scoreByFrames.push((10 + launchHistory[index + 2]))
+                    }
+                    this.playersInformations[playerNumero].index += 2;
+                    td[scoreByFrames.length - 1].innerHTML = scoreByFrames[scoreByFrames.length - 1]
                 }
                 return
             }
-
-            if (scoreTable[numberOfFrame] == "X") { // (STRIKES)
-
-                if (scoreTable[numberOfFrame + 2] == "X") {
-                    if (scoreTable[numberOfFrame + 4] == "X") {
-                        scoreByFrames.push((30))
-                        td.innerHTML = scoreByFrames[scoreByFrames.length - 1]
-                    } else if (scoreTable[numberOfFrame + 4] != null) {
-                        scoreByFrames.push((20 + scoreTable[numberOfFrame + 4]))
-                        td.innerHTML = scoreByFrames[scoreByFrames.length - 1]
-                    }
-                    return
-                }
-
-                if (scoreTable[numberOfFrame + 2] != null && scoreTable[numberOfFrame + 3] != null) {
-                    scoreByFrames.push((10 + scoreTable[numberOfFrame + 2] + scoreTable[numberOfFrame + 3]))
-                    td.innerHTML = scoreByFrames[scoreByFrames.length - 1]
-                    return
-                }
-                return
-            }
-
-            scoreByFrames.push((scoreTable[numberOfFrame] + scoreTable[numberOfFrame + 1]))
-            td.innerHTML = scoreByFrames[scoreByFrames.length - 1]
-            console.log(scoreByFrames)
+            scoreByFrames.push((launchHistory[index] + launchHistory[index + 1]))
+            this.playersInformations[playerNumero].index += 2;
+            td[scoreByFrames.length - 1].innerHTML = scoreByFrames[scoreByFrames.length - 1]
         }
     }
 
