f170428074d49123c1560f702d7e9bc309e4d7e4
Nathan <nathan.altomare.belier@gmail.com>
2021-03-20 13:09:19 +0100

    A lot of change

--- js/bowlingGameManager.js
+++ js/bowlingGameManager.js
@@ -1,101 +1,149 @@
 class bowlingGameManager {
     constructor() {
-        this.scoreCalculator = new scoreCalculator();
-        this.scoreboard = new scoreboard();
-        this.nameRegister = new nameRegister();
-        this.formRequest = new formRequest();
-        this.errorMessage = document.getElementById("errorMessage");
-        this.numberOfPlayers = 0;
-        this.playersInformations = [];
+        this.scoreCalculator = new scoreCalculator()
+        this.scoreboard = new scoreboard()
+        this.nameRegister = new nameRegister()
+        this.formRequest = new formRequest()
+        this.messageDisplayer = new messageDisplayer()
+        this.numberOfPlayers = 0
+        this.playersInformations = []
     }
 
     setStartButtonHandler() {
         this.formRequest.buttonStart.addEventListener("click", (event) => {
-            this.requestTheNumbersOfPlayers();
-            this.formRequest.buttonStart.style.display = "none";
-            event.preventDefault();
-        });
+            this.requestTheNumbersOfPlayers()
+            this.formRequest.buttonStart.style.display = "none"
+            event.preventDefault()
+        })
     }
 
     requestTheNumbersOfPlayers() {
-        this.formRequest.form.style.display = "flex";
+        this.formRequest.form.style.display = "flex"
         this.formRequest.submitButton.addEventListener("click", (event) => {
-            this.numberOfPlayers = this.formRequest.select.value;
-            this.formRequest.form.style.display = "none";
-            this.nameRegister.displayNameFields(this.numberOfPlayers);
-            this.setSubmitButtonHandler();
-            event.preventDefault();
-        });
+            this.numberOfPlayers = this.formRequest.select.value
+            this.formRequest.form.style.display = "none"
+            this.nameRegister.displayNameFields(this.numberOfPlayers)
+            this.setSubmitButtonHandler()
+            event.preventDefault()
+        })
     }
 
     setSubmitButtonHandler() {
         this.nameRegister.submitButton.addEventListener("click", (event) => {
             if (this.nameRegister.checkIfThePlayersNamesAreValid(this.numberOfPlayers) === false) {
-                this.errorMessage.innerHTML = "Il y a un problème avec un pseudo!";
+                this.messageDisplayer.errorMessage.innerHTML = "Il y a un problème avec un pseudo!"
             } else {
-                this.playersInformations = this.scoreboard.createThePlayerInformation(this.numberOfPlayers);
-                this.displayTheScoreboardOfEachPlayer();
-                this.errorMessage.innerHTML = "";
-                this.nameRegister.form.style.display = "none";
+                this.playersInformations = this.scoreboard.createThePlayerInformation(this.numberOfPlayers)
+                this.displayTheScoreboardOfEachPlayer()
+                this.messageDisplayer.errorMessage.innerHTML = ""
+                this.nameRegister.form.style.display = "none"
             }
-            event.preventDefault();
+            event.preventDefault()
         })
     }
 
     displayTheScoreboardOfEachPlayer() {
         for (let playerNumero = 0; playerNumero < this.numberOfPlayers; playerNumero++) {
-            this.scoreboard.displayedNames[playerNumero].innerHTML = this.playersInformations[playerNumero].name;
-            this.scoreboard.scoreboards[playerNumero].style.display = "block";
-            this.setAbandonButtonHandler();
+            this.scoreboard.displayedNames[playerNumero].innerHTML = this.playersInformations[playerNumero].name
+            this.scoreboard.scoreboards[playerNumero].style.display = "block"
 
-            this.scoreboard.buttonAddThrows[playerNumero].addEventListener("click", (event) => {
-
-                const throwHistory = this.playersInformations[playerNumero].throwHistory;
-                const IndexOfSlotToFill = this.scoreboard.defineTheIndexOfSlotToFill(playerNumero);
-                const playerCanPlay = this.scoreCalculator.checkIfThePlayerCanPlay(IndexOfSlotToFill, throwHistory);
-                const previousThrow = throwHistory[throwHistory.length - 1];
-                const itIsTheSecondThrow = this.scoreCalculator.checkIfItIsTheSecondThrow(IndexOfSlotToFill);
-                const score = parseInt(this.scoreboard.scoreSelect[playerNumero].value);
-                const validScore = this.scoreCalculator.checkIfThePlayerCanEnterThisScore(score, previousThrow, IndexOfSlotToFill);
-
-                if (playerCanPlay === false) {
-                    this.errorMessage.innerHTML = "Vous avez atteint le nombre maximal de lancer";
-                } else if (itIsTheSecondThrow && validScore == false) {
-                    this.errorMessage.innerHTML = "Vous avez ne pouvez pas faire tomber autant de quille";
-                } else {
-                    this.playTheThrow(itIsTheSecondThrow, score, IndexOfSlotToFill, playerNumero, throwHistory);
-                }
-
-                event.preventDefault();
-            });
+            this.setAbandonButtonHandler()
+            this.setTheButtonAddThrow(playerNumero)
         }
     }
 
-    playTheThrow(itIsTheSecondThrow, score, IndexOfSlotToFill, playerNumero, throwHistory) {
-        const playerThrow = this.scoreCalculator.returnThePlayerThrow(itIsTheSecondThrow, score, IndexOfSlotToFill, this.playersInformations[playerNumero]);
+    setTheButtonAddThrow(playerNumero) {
+        this.scoreboard.buttonAddThrows[playerNumero].addEventListener("click", (event) => {
+            const throwInformation = this.scoreboard.defineAThrowInformation(this.playersInformations[playerNumero].throwHistory, playerNumero)
+            const frameInformation = this.scoreboard.defineAframeInformation(throwInformation, this.playersInformations[playerNumero])
+            const checkCondition = this.scoreCalculator.checkCondition(throwInformation)
+            const playerCantPlay = this.scoreCalculator.determinateIfPlayerCantDoTheThrow(checkCondition)
 
-        this.scoreboard.displayThePlayerThrow(playerNumero, IndexOfSlotToFill, playerThrow);
+            if (playerCantPlay != null) {
+                this.messageDisplayer.errorMessage.innerHTML = playerCantPlay
+            } else {
+                const playerThrow = this.manageThePlayerThrow(checkCondition, throwInformation)
+                const frameThrow = this.manageTheFrameThrow(frameInformation, playerNumero)
+                const totalScore = this.manageTheTotalScore(throwInformation)
 
-        this.scoreCalculator.pushTheScoreInTheThrowHistory(throwHistory, playerThrow);
+                this.scoreboard.updateTheScoreboardOnTheScreen(throwInformation, playerThrow, frameThrow, frameInformation.frameHistory, totalScore)
+                this.tryToFinishTheGame(throwInformation)
+            }
+            event.preventDefault()
+        })
+    }
 
-        const indexOfTheFirstThrowOfTheCurrentFrame = this.playersInformations[playerNumero].indexOfTheFirstThrowOfTheCurrentFrame;
+    manageTheFrameThrow(frameInformation, playerNumero) {
+        const frameThrow = this.scoreCalculator.returnTheFrameScore(frameInformation.indexOfTheFirstThrowOfTheCurrentFrame, this.playersInformations[playerNumero])
+        this.scoreCalculator.pushTheScoreInTheFrameHistory(frameInformation.frameHistory, frameThrow)
+        return frameThrow
+    }
 
-        const frameHistory = this.playersInformations[playerNumero].frameHistory;
+    manageThePlayerThrow(checkCondition, throwInformation) {
+        const playerThrow = this.scoreCalculator.returnThePlayerThrow(checkCondition, throwInformation)
+        this.scoreCalculator.pushTheScoreInTheThrowHistory(throwInformation.throwHistory, playerThrow)
+        return playerThrow
+    }
 
-        const frameThrow = this.scoreCalculator.returnTheFrameScore(indexOfTheFirstThrowOfTheCurrentFrame, this.playersInformations[playerNumero]);
+    manageTheTotalScore(throwInformation) {
+        const totalScore = this.scoreCalculator.calculateActualTotalScore(this.playersInformations[throwInformation.playerNumero])
+        this.playersInformations[throwInformation.playerNumero].totalScore = totalScore
+        return totalScore
+    }
 
-        this.scoreboard.showFrameScore(playerNumero, frameThrow, frameHistory);
-
-        const totalScore = this.scoreCalculator.calculateActualTotalScore(this.playersInformations[playerNumero]);
-
-        this.scoreboard.showActualTotalScore(playerNumero, totalScore);
+    tryToFinishTheGame(throwInformation) {
+        const indexOfCurrentSlot = throwInformation.IndexOfSlotToFill
+        this.checkIfPlayerFinish(indexOfCurrentSlot, throwInformation)
+        this.checkIfAllPlayersFinish()
     }
 
     setAbandonButtonHandler() {
-        this.scoreboard.buttonAbandon.style.display = "block";
+        this.scoreboard.buttonAbandon.style.display = "block"
         this.scoreboard.buttonAbandon.addEventListener("click", (event) => {
-            document.location.reload();
-            event.preventDefault();
-        });
+            document.location.reload()
+            event.preventDefault()
+        })
+    }
+
+    checkIfPlayerFinish(IndexOfCurrentSlot, throwInformation) {
+        const lastSlotAfterNormalThrow = IndexOfCurrentSlot == 19 && (throwInformation.throwHistory[throwInformation.throwHistory.length - 1] + throwInformation.throwHistory[throwInformation.throwHistory.length - 2]) < 10
+        const lastSlotAfterStrikeOrPair = IndexOfCurrentSlot == 20
+
+        if (lastSlotAfterNormalThrow || lastSlotAfterStrikeOrPair) {
+            this.scoreboard.buttonAddThrows[throwInformation.playerNumero].disabled = true
+            this.playersInformations[throwInformation.playerNumero].endOfTheGame = true
+        }
+    }
+
+    checkIfAllPlayersFinish() {
+        let allPlayersFinished = true
+        this.playersInformations.forEach(playerInformation => {
+            if (playerInformation.endOfTheGame == false) {
+                allPlayersFinished = false
+            }
+        })
+        if (allPlayersFinished) {
+            this.showTheWinner()
+        }
+    }
+
+    showTheWinner() {
+        let betterScore = 0
+        let winners = []
+        this.playersInformations.forEach(player => {
+            if (player.totalScore > betterScore) {
+                betterScore = player.totalScore
+            }
+        })
+        this.playersInformations.forEach(player => {
+            if (player.totalScore == betterScore) {
+                winners.push(player.name)
+            }
+        })
+        if (winners.length == 1) {
+            this.messageDisplayer.winMessage.innerHTML = "Bravo le gagnant est : " + winners[0]
+        } else {
+            this.messageDisplayer.winMessage.innerHTML = "Bravo les gagnants sont : " + winners.toString()
+        }
     }
 }
--- js/scoreCalculator.js
+++ js/scoreCalculator.js
@@ -1,40 +1,41 @@
 class scoreCalculator {
     constructor() {}
 
-    returnThePlayerThrow(itIsTheSecondThrow, score, IndexOfSlotToFill, objet) {
-        const previousThrow = objet.throwHistory[objet.throwHistory.length - 1];
-        const pair = this.checkIfItIsAPair(itIsTheSecondThrow, score, previousThrow)
-        const strike = this.checkIfItIsAStrike(score, objet.throwHistory, IndexOfSlotToFill)
+    returnThePlayerThrow(checkCondition, throwInformation) {
+        const previousThrow = throwInformation.throwHistory[throwInformation.throwHistory.length - 1]
+        const pair = this.checkIfItIsAPair(checkCondition.itIsTheSecondThrow, throwInformation.score, previousThrow)
+        const strike = this.checkIfItIsAStrike(throwInformation.score, throwInformation.throwHistory, throwInformation.IndexOfSlotToFill)
+        let score = throwInformation.score
 
         if (pair) {
             score = "/"
         } else if (strike) {
             score = "X"
         }
-        return score;
+        return score
     }
 
     checkIfThePlayerCanPlay(IndexOfSlotToFill, throwHistory) {
-        let playerCanDoTheNextThrow = true;
+        let playerCanDoTheNextThrow = true
         if (IndexOfSlotToFill === 20) {
             if ((throwHistory[throwHistory.length - 1] + throwHistory[throwHistory.length - 2]) < 10) {
                 playerCanDoTheNextThrow = false
             }
-            return playerCanDoTheNextThrow
         }
+        return playerCanDoTheNextThrow
     }
 
     checkIfItIsAStrike(score, throwHistory, IndexOfSlotToFill) {
-        let itIsAStrike = false;
+        let itIsAStrike = false
 
         if (score === 10) {
             if (throwHistory[throwHistory.length - 1] === 0 && (IndexOfSlotToFill % 2) != 0) {
-                console.log("NO PROBLEM")
+                itIsAStrike = false
             } else {
-                itIsAStrike = true;
+                itIsAStrike = true
             }
         }
-        return itIsAStrike;
+        return itIsAStrike
     }
 
     checkIfItIsAPair(itIsTheSecondThrow, score, previousThrow) {
@@ -46,95 +47,118 @@ class scoreCalculator {
     }
 
     checkIfThePlayerCanEnterThisScore(score, previousThrow, IndexOfSlotToFill) {
-        let validScore = true;
+        let validScore = true
         if ((score + previousThrow) > 10 || previousThrow === "X" && IndexOfSlotToFill < 18) {
-            validScore = false;
+            validScore = false
         }
-        return validScore;
+        return validScore
     }
 
     pushTheScoreInTheThrowHistory(throwHistory, score) {
-        throwHistory.push(score);
+        if (score == "/") {
+            score = 10 - throwHistory[throwHistory.length - 1]
+        }
+        throwHistory.push(score)
+    }
+
+    pushTheScoreInTheFrameHistory(frameHistory, frameThrow) {
+        if (frameThrow != undefined) {
+            frameHistory.push(frameThrow)
+        }
     }
 
     checkIfItIsTheSecondThrow(IndexOfSlotToFill) {
-        let itIsTheSecondThrow = false;
+        let itIsTheSecondThrow = false
         if (IndexOfSlotToFill % 2 != 0) {
-            itIsTheSecondThrow = true;
+            itIsTheSecondThrow = true
         }
-        return itIsTheSecondThrow;
+        return itIsTheSecondThrow
     }
 
-    returnTheFrameScore(indexOfTheFirstThrowOfTheCurrentFrame, objet) {
-        const firstThrow = objet.throwHistory[indexOfTheFirstThrowOfTheCurrentFrame];
-        const secondThrow = objet.throwHistory[indexOfTheFirstThrowOfTheCurrentFrame + 1];
-        const thirdThrow = objet.throwHistory[indexOfTheFirstThrowOfTheCurrentFrame + 2];
-        const currentFrame = firstThrow + secondThrow;
+    returnTheFrameScore(indexOfTheFirstThrowOfTheCurrentFrame, playerInformation) {
+        const firstThrow = playerInformation.throwHistory[indexOfTheFirstThrowOfTheCurrentFrame]
+        const secondThrow = playerInformation.throwHistory[indexOfTheFirstThrowOfTheCurrentFrame + 1]
+        const thirdThrow = playerInformation.throwHistory[indexOfTheFirstThrowOfTheCurrentFrame + 2]
+        const currentFrame = firstThrow + secondThrow
+        let score = undefined
 
-        let canNotCalculate = false;
-        let score = undefined;
+        const conditionForCalculateAStrike = firstThrow === "X" && secondThrow != null && thirdThrow != null
+        const conditionForCalculateAPair = firstThrow != null && secondThrow != null && thirdThrow != null && currentFrame === 10
+        const conditionForCalculANormalFrame = firstThrow != null && secondThrow != null && currentFrame != 10 && Number.isInteger(currentFrame)
 
-        if (firstThrow === "X") {
-            if (secondThrow != null && thirdThrow != null) {
-                score = this.defineStrikeScore(objet, indexOfTheFirstThrowOfTheCurrentFrame);
-            } else {
-                canNotCalculate = true;
-            }
-        }
-
-        if (firstThrow != null && secondThrow != null) {
-            if (currentFrame === 10) {
-                if (thirdThrow != null) {
-                    score = this.defineScoreWithTheThrirdThrow(thirdThrow, objet);
-                } else {
-                    canNotCalculate = true;
-                }
-            }
+        if (conditionForCalculateAStrike) {
+            score = this.defineStrikeScore(playerInformation)
+            playerInformation.indexOfTheFirstThrowOfTheCurrentFrame += 1
+        } else if (conditionForCalculateAPair) {
+            score = this.defineScoreWithTheThrirdThrow(thirdThrow, playerInformation)
+            playerInformation.indexOfTheFirstThrowOfTheCurrentFrame += 2
+        } else if (conditionForCalculANormalFrame) {
+            score = currentFrame
+            playerInformation.indexOfTheFirstThrowOfTheCurrentFrame += 2
         } else {
-            canNotCalculate = true
+            score = undefined
         }
 
-        if (canNotCalculate == true) {
-            score = undefined
-        } else if (score == undefined) {
-            objet.frameHistory.push(currentFrame)
-            objet.indexOfTheFirstThrowOfTheCurrentFrame += 2;
-            score = objet.frameHistory[objet.frameHistory.length - 1]
-        }
         return score
     }
 
-    defineScoreWithTheThrirdThrow(thirdThrow, objet) {
+    defineScoreWithTheThrirdThrow(thirdThrow, playerInformation) {
+        let sumOfFrameScores = 0
         if (thirdThrow === "X") {
-            objet.frameHistory.push(20);
+            sumOfFrameScores = 20
         } else {
-            objet.frameHistory.push((10 + thirdThrow));
+            sumOfFrameScores = (10 + thirdThrow)
         }
-        objet.indexOfTheFirstThrowOfTheCurrentFrame += 2;
-        return objet.frameHistory[objet.frameHistory.length - 1];
+        return sumOfFrameScores
     }
 
-    defineStrikeScore(objet, indexOfTheFirstThrowOfTheCurrentFrame) {
-        let sumOfFrameScores = 0;
+    defineStrikeScore(playerInformation) {
+        let sumOfFrameScores = 0
         for (let i = 0; i < 3; i++) {
-            if (objet.throwHistory[indexOfTheFirstThrowOfTheCurrentFrame + i] === "X") {
-                sumOfFrameScores += 10;
+            if (playerInformation.throwHistory[playerInformation.indexOfTheFirstThrowOfTheCurrentFrame + i] === "X") {
+                sumOfFrameScores += 10
             } else {
-                sumOfFrameScores += objet.throwHistory[indexOfTheFirstThrowOfTheCurrentFrame + i];
+                sumOfFrameScores += playerInformation.throwHistory[playerInformation.indexOfTheFirstThrowOfTheCurrentFrame + i]
             }
         }
-        objet.frameHistory.push(sumOfFrameScores);
-        objet.indexOfTheFirstThrowOfTheCurrentFrame += 1;
-        return objet.frameHistory[objet.frameHistory.length - 1];
+        return sumOfFrameScores
     }
 
-    calculateActualTotalScore(objet) {
-        const frameHistory = objet.frameHistory;
-        let totalScore = 0;
+    calculateActualTotalScore(playerInformation) {
+        const frameHistory = playerInformation.frameHistory
+        let totalScore = 0
 
         for (let i = 0; i < frameHistory.length; i++) {
             totalScore += frameHistory[i]
         }
-        return totalScore;
+        return totalScore
+    }
+
+    determinateDate() {
+        let currentDateInformations = new Date()
+        let month = (currentDateInformations.getMonth() + 1)
+        if (month < 10) {
+            month = "0" + month
+        }
+        return (currentDateInformations.getDate() + "/" + month + "/" + currentDateInformations.getFullYear())
+    }
+
+    checkCondition(throwInformation) {
+        return {
+            itIsTheSecondThrow: this.checkIfItIsTheSecondThrow(throwInformation.IndexOfSlotToFill),
+            playerCanPlay: this.checkIfThePlayerCanPlay(throwInformation.IndexOfSlotToFill, throwInformation.throwHistory),
+            validScore: this.checkIfThePlayerCanEnterThisScore(throwInformation.score, throwInformation.previousThrow, throwInformation.IndexOfSlotToFill)
+        }
+    }
+
+    determinateIfPlayerCantDoTheThrow(checkCondition) {
+        let errorMessage = null
+
+        if (checkCondition.playerCanPlay === false) {
+            errorMessage = "Vous avez atteint le nombre maximal de lancer"
+        } else if (checkCondition.itIsTheSecondThrow && checkCondition.validScore == false) {
+            errorMessage = "Vous avez ne pouvez pas faire tomber autant de quille"
+        }
+        return errorMessage
     }
 }
